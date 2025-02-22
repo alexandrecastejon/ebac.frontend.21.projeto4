0ab1177de6472eef3dced572b67311ffd6fd9ce6
Alexandre Castejon <alexandrecastejon@hotmail.com>
2025-02-22 19:02:40 -0300

    entrega do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,18 @@ const sass = require('gulp-sass')(require('sass'));
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.parallel(images));
 }
